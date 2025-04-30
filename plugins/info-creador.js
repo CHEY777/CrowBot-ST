81ac34fbd3be0f337a7b1c0df9db1a3cf51a2f5c
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2025-04-30 09:00:50 +0530

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,22 +1,27 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-   await m.react('🔮');
+    await m.react('🔮');
 
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    // Get sender info
+    let who = m.mentionedJid?.[0] || (m.fromMe ? conn.user.jid : m.sender);
     let name = await conn.getName(who);
-    let edtr = `@${m.sender.split`@`[0]}`;
-    let username = conn.getName(m.sender);
+    let username = await conn.getName(m.sender);
+    let senderUsername = m.sender.split('@')[0];
 
-    // VCARD
+    // Developer Info
+    let dev = "ᴄʜᴇʏ_ᴄᴏᴅᴇx🫧";
+
+    // VCARD contact list
     let list = [{
         displayName: "ᥴꫝꫀꪗ-𝙎𝙖𝙣💫🌙✨",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: ᴄʜᴇʏ_ᴄᴏᴅᴇx🍭\nitem1.TEL;waid=918116781147:918116781147\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: chaitanya811678@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/its_chey7\nitem3.X-ABLabel:Internet\nitem4.ADR:;; India;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${dev}\nTEL;waid=918116781147:+91 81167 81147\nEMAIL;type=INTERNET:chaitanya811678@gmail.com\nURL:https://www.instagram.com/its_chey7\nADR:;;India;;;;\nEND:VCARD`
     }];
 
+    // Send contact card
     await conn.sendMessage(m.chat, {
         contacts: {
-            displayName: `${list.length} Contacto`,
+            displayName: `${list.length} Contact`,
             contacts: list
         },
         contextInfo: {
@@ -30,15 +35,14 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
                 renderLargerThumbnail: true
             }
         }
-    }, {
-        quoted: mcontacto
     });
 
-    let txt = `👋 *hello-hello \`${username}\` this is*\n*my creator's contact*`;
-
-    await conn.sendMessage(m.chat, { text: txt });
+    // Send text message
+    let txt = `👋 *Hello \`${username}\`, this is my creator's contact card.*\nFeel free to connect!`;
+    await conn.sendMessage(m.chat, { text: txt }, { quoted: m });
 };
 
+// Handler config
 handler.help = ['owner', 'creator'];
 handler.tags = ['main'];
 handler.command = /^(owner|creator|creador|dueño|malik|baby)$/i;
