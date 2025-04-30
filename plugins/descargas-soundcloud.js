5c97fd936183e6099aae725985808d39f8926524
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2025-04-30 07:38:47 +0530

    Update descargas-soundcloud.js

--- plugins/descargas-soundcloud.js
+++ plugins/descargas-soundcloud.js
@@ -1,18 +1,18 @@
-// By WillZek >> https://github.com/WillZek
+// By chey >> https://github.com/Cheysan
 
 import fetch from 'node-fetch';
 import fg from 'senna-fg';
 
 let handler = async(m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command} crow edits`);
+if (!text) return m.reply(`🍭 Enter Text To Search On Youtube\n> *Example:* ${usedPrefix + command} chey edits`);
 
 try {
 let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
 
 let results = api.data[0];
 
-let txt = `✨ *Título:* ${results.title}\n⌛ *Duración:* ${results.duration}\n📎 *Link:* ${results.url}\n📆 *Publicado:* ${results.publishedAt}`;
+let txt = `✨ *Title:* ${results.title}\n⌛ *Duration:* ${results.duration}\n📎 *Link:* ${results.url}\n📆 *Published:* ${results.publishedAt}`;
 
 let img = results.image;
 
@@ -20,7 +20,7 @@ conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
 
 /* let api2 = await(await fetch(`https://api.neoxr.eu/api/youtube?url=${results.url}&type=audio&quality=128kbps&apikey=GataDios`)).json();
 
-if (!api2?.data.url) return m.reply('No Se  Encontraron Resultados');
+if (!api2?.data.url) return m.reply('No Results Found');
 */
 
 let api2 = await(await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
@@ -28,7 +28,7 @@ let api2 = await(await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.u
 conn.sendMessage(m.chat, { audio: { url: api2.result.download.url }, mimetype: 'audio/mpeg' }, { quoted: m });
 
 } catch (e) {
-m.reply(`*No Encontramos Resultados Para Tu Búsqueda* ${e.message}\n> Posdata: Me Pica Un Webo`);
+m.reply(`*We Didn't Find Any Results For Your Search* ${e.message}\n> Posdata: 🥺`);
 m.react('✖️');
   }
 }
