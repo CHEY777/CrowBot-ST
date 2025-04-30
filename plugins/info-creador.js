0bbfb4f57438945a6471f4ee145a201a47cefcac
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2025-04-30 06:43:31 +0530

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-   await m.react('🎩');
+   await m.react('🔮');
 
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = await conn.getName(who);
@@ -10,8 +10,8 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 
     // VCARD
     let list = [{
-        displayName: "WillZek-Ofc 🎩",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: ᴡɪʟʟᴢᴇᴋ-ᴏғɪᴄɪᴀʟ🍭\nitem1.TEL;waid=50557865603:50557865603\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: ninopina10@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/crowbot_wa\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Nicaragua;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+        displayName: "ᥴꫝꫀꪗ-𝙎𝙖𝙣💫🌙✨",
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: ᴄʜᴇʏ_ᴄᴏᴅᴇx🍭\nitem1.TEL;waid=918116781147:918116781147\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: chaitanya811678@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/its_chey7\nitem3.X-ABLabel:Internet\nitem4.ADR:;; India;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
     }];
 
     await conn.sendMessage(m.chat, {
@@ -22,25 +22,25 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
         contextInfo: {
             externalAdReply: {
                 showAdAttribution: true,
-                title: 'һ᥆ᥣᥲ s᥆ᥡ ᥕіᥣᥣzᥱk-᥆𝖿ᥴ ᥱᥣ mᥱȷ᥆r',
+                title: '𝓗𝓮𝓵𝓵𝓸, 𝓘𝓶 𝓬𝓱𝓮𝔂, 𝓽𝓱𝓮 𝓫𝓮𝓼𝓽',
                 body: dev,
-                thumbnailUrl: 'https://cdnmega.vercel.app/media/c4hhgZgD@fGOHhRAM1CD-3_cpAQk-Q86yQnQLGHYKZ1M0P_heI9s',
-                sourceUrl: 'https://github.com/WillZek',
+                thumbnailUrl: 'https://files.catbox.moe/hofuch.jpg',
+                sourceUrl: 'https://github.com/Chey-san',
                 mediaType: 1,
                 renderLargerThumbnail: true
             }
         }
     }, {
-        quoted: m
+        quoted: mcontacto
     });
 
-    let txt = `👋 *Hola \`${username}\` este es*\n*el contacto de mi creador*`;
+    let txt = `👋 *hello-hello \`${username}\` this is*\n*my creator's contact*`;
 
     await conn.sendMessage(m.chat, { text: txt });
 };
 
 handler.help = ['owner', 'creator'];
 handler.tags = ['main'];
-handler.command = /^(owner|creator|creador|dueño)$/i;
+handler.command = /^(owner|creator|creador|dueño|malik|baby)$/i;
 
 export default handler;
